4a8e750e715a23de2274462f3adbb86af14a37ae
asadahmed01 <asadatnish@gmail.com>
2024-02-15 17:39:28 -0500

    feat: add edit link for the url

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,9 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: [:show]
+  before_action :set_link, only: [:show, :edit, :update, :destroy]
+
   def index
     @links = Link.recent_first
+    @link ||= Link.new
   end
 
   def show
@@ -15,6 +17,21 @@ class LinksController < ApplicationController
       index
       render :index, status: unprocessable_entity
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @link.update(link_params)
+      redirect_to @link
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+
   end
 
 
