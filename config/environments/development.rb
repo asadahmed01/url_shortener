f43365cfc81fe8f7a92de98517f7aa912c8dd7b8
asad ahmed <asadatnish@gmail.com>
2024-03-23 12:33:52 -0400

    add postmark as email delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,16 +78,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: '<asadatnish@gmail.com>'}
+  config.action_mailer.default_url_options = { host: '10xgx.hatchboxapp.com' }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'localhost',
-  user_name:            Rails.application.credentials[:sender_email],
-  password:             Rails.application.credentials[:gmail_password],
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = { api_token: Rails.application.credentials.postmark_api_token }
 
 end
